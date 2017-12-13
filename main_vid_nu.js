36e9f3eaf73cd4ba5f983c55c7072c6762800924
Roland Arnoldt <rolandarnoldt@172-16-253-134.DYNAPOOL.NYU.EDU>
2017-12-13 16:29:24 -0500

    updated main JS code with python txt workaround

--- main_vid_nu.js
+++ main_vid_nu.js
@@ -1,5 +1,17 @@
+// run 'python3 -m http-server'
+// edit 'move_files.py', replace folder names with local folder structure
+// run 'move_files.py' in background to move around recorded video bits (use sudo if needed)
+// (move_files.py circumvents data loss when reloading pages between users by storing values in move_vids.txt file)
+// script uses quite a bit of webRTC, stream from webcam might initialise too slow - reload page if needed
+// still a few bugs with loading the video files - it takes sometimes a few seconds to load in new videos
+
+//source-code:
+// recording from webRCT - code based on https://github.com/webrtc/samples/tree/gh-pages/src/content/getusermedia/record
+// video screen - code based on https://github.com/mrdoob/three.js/blob/dev/examples/canvas_materials_video.html
+
 var AMOUNT = 100;
 var container, stats;
+var counter = 0;
 var camera, scene, renderer;
 var videostream, videoImage, videoImageContext, videoTexture;
 var video, beat, image, imageContext,
@@ -13,33 +25,10 @@ var mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-//// distortion elements
+///////////////////////////
+//webRTC rec controls/////
+/////////////////////////
 
-// var video4, texture4, material4, mesh4;
-//
-// var composer;
-//
-// var mouseX = 0;
-// var mouseY = 0;
-//
-// var windowHalfX = window.innerWidth / 2;
-// var windowHalfY = window.innerHeight / 2;
-//
-// var cube_count,
-//
-//   meshes = [],
-//   materials = [],
-//
-//   xgrid = 40,
-//   ygrid = 20;
-/////// end of distoprtion elements
-
-
-
-
-////////////////////
-//rec controls/////
-//////////////////
 var mediaSource = new MediaSource();
 mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
 var mediaRecorder;
@@ -55,12 +44,6 @@ var sourceBuffer;
 var gumVideo = document.querySelector('video#gum');
 var recordedVideo = document.querySelector('video#recorded');
 
-//var recordButton = document.querySelector('button#record');
-//var playButton = document.querySelector('button#play');
-//var downloadButton = document.querySelector('button#download');
-//recordButton.onclick = toggleRecording;
-//playButton.onclick = play;
-//downloadButton.onclick = download;
 
 // window.isSecureContext could be used for Chrome
 var isSecureOrigin = location.protocol === 'https:' ||
@@ -116,16 +99,11 @@ function toggleRecording() {
     console.log('yo');
     startRecording();
     rec_time();
-    //stopRecording();
-    //recordButton.textContent = 'Start Recording';
-    //playButton.disabled = false;
-    //downloadButton.disabled = false;
 }
 
 function rec_time() {
     setTimeout(function(){
       stopRecording();
-      //playButton.disabled = false;
       play();
     }, 40000);
     console.log('stopped recording');
@@ -156,9 +134,7 @@ function startRecording() {
       return;
     }
     console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-    //recordButton.textContent = 'Stop Recording';
-    //playButton.disabled = true;
-    //downloadButton.disabled = true;
+
     mediaRecorder.onstop = handleStop;
     mediaRecorder.ondataavailable = handleDataAvailable;
     mediaRecorder.start(10); // collect 10ms of data
@@ -171,9 +147,6 @@ function stopRecording() {
   console.log('Recorded Blobs: ', recordedBlobs);
   recordedVideo.controls = true;
   download_popup();
-  // setTimeout(function(){
-  //   window.location.href = "index.html";
-  // }, 15000);
 }
 
 function download_popup() {
@@ -188,7 +161,7 @@ function download_popup() {
           window.location.href = "index.html";
       }
       document.getElementById("demo").innerHTML = txt;
-    }, 15000);
+    }, 25000);
     console.log('  recording, going back home');
 }
 
@@ -211,24 +184,51 @@ function play() {
   });
 }
 
+// read move_vids.txt that stores the name of the last downloaded video
+// this dynamically renames the downloads alternating between left and right channel
+function readTextFile(file)
+{
+    return new Promise(function(resolve, reject) {
+      var rawFile = new XMLHttpRequest();
+      rawFile.open("GET", file, false);
+      rawFile.onreadystatechange = function ()
+      {
+          if(rawFile.readyState === 4)
+          {
+              if(rawFile.status === 200 || rawFile.status == 0)
+              {
+                  var allText = rawFile.responseText;
+                  resolve(allText);
+                  //alert(allText);
+              }
+          }
+          // reject();
+      }
+      rawFile.send(null);
+    });
+}
+
 function download() {
   var blob = new Blob(recordedBlobs, {type: 'video/webm'});
   var url = window.URL.createObjectURL(blob);
   var a = document.createElement('a');
   a.style.display = 'none';
   a.href = url;
-  if ( repl_left.readyState === repl_left.HAVE_ENOUGH_DATA ){
-    a.download = 'repl_right_nu.webm';
-  }
-  else {
-    a.download = 'repl_left_nu.webm';
-  }
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(function() {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 100);
+  readTextFile("move_vids.txt").then(function(vid_indicator) {
+    console.log (vid_indicator);
+    if ( vid_indicator === "file left" ){
+      a.download = 'repl_right_nu.webm';
+    }
+    else {
+      a.download = 'repl_left_nu.webm';
+    }
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 100);
+  });
 }
 
 
@@ -254,50 +254,32 @@ function init() {
 
   // here we grab the videos from their html elements:
   videostream = document.getElementById('monitor')
-  //video = document.getElementById( 'video' );
   video2 = document.getElementById( 'video2' );
   video3 = document.getElementById( 'video3' );
   console.log(video3);
   repl_left = document.getElementById( 'repl_left' );
   repl_right = document.getElementById( 'repl_right' );
-  //console.log (downloaded_vid);
-  ///////////
-	// stream from webcam //
-	///////////
+
+    ////////////////////////
+	 // stream from webcam //
+	////////////////////////
 
 	videoImage = document.getElementById( 'videoImage' );
   videoImage.width = 480;
   videoImage.height = 204;
 	videoImageContext = videoImage.getContext( '2d' );
-	// background color if no video present
-	//videoImageContext.fillStyle = '#000000';
-	//videoImageContext.fillRect( 0, 0, 480, 204 );
 
 	videoTexture = new THREE.Texture( videoImage );
 	videoTexture.minFilter = THREE.LinearFilter;
 	videoTexture.magFilter = THREE.LinearFilter;
 
-	// var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
-	// // the geometry on which the movie will be displayed;
-	// // 		movie image will be scaled to fit these dimensions.
-	// var movieGeometry = new THREE.PlaneGeometry( 100, 100, 1, 1 );
-	// var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
-	// movieScreen.position.set(0,0,0);
-	// scene.add(movieScreen);
-  // camera.position.set(0,150,1000);
-	// camera.lookAt(movieScreen.position);
-
-
-  //Front screen
+  //videotexture for the front screen
   image = document.createElement( 'canvas' );
   image.width = 362;
   image.height = 204;
   imageContext = image.getContext( '2d' );
   imageContext.fillStyle = '#000000';
   imageContext.fillRect( 0, 0, 480, 204 );
-
-
-  //videotexture for the front screen
   texture = new THREE.Texture( videoImage );
   var material = new THREE.MeshBasicMaterial( {map:videoTexture, overdraw: 0.5} );
   var plane = new THREE.PlaneGeometry( 380, 204, 4, 4 );
@@ -306,14 +288,13 @@ function init() {
   scene.add(mesh);
 
 
-  //left screen
+  //videotexture for the left screen
   image2 = document.createElement( 'canvas' );
   image2.width = 480;
   image2.height = 204;
   imageContext2 = image2.getContext( '2d' );
   imageContext2.fillStyle = '#000000';
   imageContext2.fillRect( 0, 0, 480, 204 );
-
   texture2 = new THREE.Texture( image2 );
   var material = new THREE.MeshBasicMaterial( {map: texture2, overdraw: 0.5} );
   let mesh2 = new THREE.Mesh( plane, material);
@@ -324,14 +305,13 @@ function init() {
   mesh2.scale.x = mesh.scale.y = mesh.scale.z = 1.6;
   scene.add(mesh2);
 
-  //right fullScreen
+  //videotexture for the right screen
   image3 = document.createElement( 'canvas' );
   image3.width = 362;
   image3.height = 204;
   imageContext3 = image3.getContext( '2d' );
   imageContext3.fillStyle = '#000000';
   imageContext3.fillRect( 0, 0, 480, 204 );
-
   texture3 = new THREE.Texture( image3 );
   var material = new THREE.MeshBasicMaterial( {map: texture3, overdraw: 0.5} );
   mesh3 = new THREE.Mesh( plane, material);
@@ -369,101 +349,8 @@ function init() {
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild( renderer.domElement );
-  //stats = new Stats();
-  //container.appendChild( stats.dom );
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
   window.addEventListener( 'resize', onWindowResize, false );
-
-//// distortion bits
-// var light = new THREE.DirectionalLight( 0xffffff );
-// light.position.set( 0.5, 1, 1 ).normalize();
-// scene.add( light );
-
-// renderer = new THREE.WebGLRenderer( { antialias: false } );
-// renderer.setPixelRatio( window.devicePixelRatio );
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// container.appendChild( renderer.domElement );
-//
-//
-// video4 = document.getElementById( 'video4' );
-//
-// texture4 = new THREE.VideoTexture( video4 );
-// texture4.minFilter = THREE.LinearFilter;
-// texture4.magFilter = THREE.LinearFilter;
-// texture4.format = THREE.RGBFormat;
-// //
-// var i, j, ux, uy, ox, oy,
-//   geometry,
-//   xsize, ysize;
-//
-// ux = 1 / xgrid;
-// uy = 1 / ygrid;
-//
-// xsize = 480 / xgrid;
-// ysize = 204 / ygrid;
-//
-// var parameters = { color: 0xffffff, map: texture };
-//
-// cube_count = 0;
-//
-// for ( i = 0; i < xgrid; i ++ )
-// for ( j = 0; j < ygrid; j ++ ) {
-//
-//   ox = i;
-//   oy = j;
-//
-//   geometry = new THREE.BoxGeometry( xsize, ysize, xsize );
-//
-//   //change_uvs( geometry, ux, uy, ox, oy );
-//
-//   materials[ cube_count ] = new THREE.MeshLambertMaterial( parameters );
-//
-//   material4 = materials[ cube_count ];
-//
-//   material4.hue = i/xgrid;
-//   material4.saturation = 1 - j/ygrid;
-//
-//   //material.color.setHSL( material.hue, material.saturation, 0.5 );
-//
-//   mesh4 = new THREE.Mesh( geometry, material4 );
-//
-//   mesh4.position.x =   ( i - xgrid/2 ) * xsize;
-//   mesh4.position.y =   ( j - ygrid/2 ) * ysize;
-//   mesh4.position.z = 0;
-//
-//   mesh4.scale.x = mesh4.scale.y = mesh4.scale.z = 1;
-//
-//   scene.add( mesh4 );
-//
-//   mesh4.dx = 0.001 * ( 0.5 - Math.random() );
-//   mesh4.dy = 0.001 * ( 0.5 - Math.random() );
-//
-//   meshes[ cube_count ] = mesh4;
-//
-//   cube_count += 1;
-//
-//   renderer.autoClear = false;
-//
-//   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-//
-//   // postprocessing
-//
-//   var renderModel = new THREE.RenderPass( scene, camera );
-//   //var effectBloom = new THREE.BloomPass( 1.3 );
-//   var effectCopy = new THREE.ShaderPass( THREE.CopyShader );
-//
-//   effectCopy.renderToScreen = true;
-//
-//   composer = new THREE.EffectComposer( renderer );
-//
-//   composer.addPass( renderModel );
-//   //composer.addPass( effectBloom );
-//   composer.addPass( effectCopy );
-//
-//   window.addEventListener( 'resize', onWindowResize, false );
-
- /////// end of distortion bits
-// }
 
 }
 function onWindowResize() {
@@ -475,34 +362,8 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
-  //composer.reset();
 
 }
-
-/// distortion extras
-
-// function change_uvs( geometry, unitx, unity, offsetx, offsety ) {
-//
-//   var faceVertexUvs = geometry.faceVertexUvs[ 0 ];
-//
-//   for ( var i = 0; i < faceVertexUvs.length; i ++ ) {
-//
-//     var uvs = faceVertexUvs[ i ];
-//
-//     for ( var j = 0; j < uvs.length; j ++ ) {
-//
-//       var uv = uvs[ j ];
-//
-//       uv.x = ( uv.x + offsetx ) * unitx;
-//       uv.y = ( uv.y + offsety ) * unity;
-//
-//     }
-//
-//   }
-//
-// }
-
-//// endof distorrtion extras
 
 function onDocumentMouseMove( event ) {
   mouseX = ( event.clientX - windowHalfX );
@@ -512,12 +373,9 @@ function onDocumentMouseMove( event ) {
 function animate() {
   requestAnimationFrame( animate );
   render();
-  //stats.update();
-  //toggleRecording();
-
 }
 
-var h, counter = 1;
+var h;
 
 function render() {
 
@@ -539,7 +397,7 @@ function render() {
     if ( texture2 ) texture2.needsUpdate = true;
   }
 
-  //replace video2 with the recorded or video if it is there
+  //replace video2 and 3 with the fresh recorded videos if they are there, otherwise play pre-recorded vids
   if ( repl_left.readyState === repl_left.HAVE_ENOUGH_DATA ) {
       imageContext2.drawImage( repl_left, 0, 0, videoImage.width, videoImage.height );
       texture2.needsUpdate = false;
@@ -568,54 +426,7 @@ function render() {
 		if ( videoTexture )
 			videoTexture.needsUpdate = true;
 	}
-  //renderer.render( scene, camera );
-///////distortion bits
-// for ( var i = 0; i < cube_count; i ++ ) {
-//
-//   material4 = materials[ i ];
-//
-//   h = ( 360 * ( material4.hue + time ) % 360 ) / 360;
-//   //material.color.setHSL( h, material.saturation, 0.5 );
-//
-// }
-//
-// if ( counter % 1000 > 200 ) {
-//
-//   for ( var i = 0; i < cube_count; i ++ ) {
-//
-//     mesh4 = meshes[ i ];
-//
-//     mesh4.rotation.x += 10 * mesh.dx;
-//     mesh4.rotation.y += 10 * mesh.dy;
-//
-//     mesh4.position.x += 200 * mesh.dx;
-//     mesh4.position.y += 200 * mesh.dy;
-//     mesh4.position.z += 400 * mesh.dx;
-//
-//   }
-//
-// }
-//
-// if ( counter % 1000 === 0 ) {
-//
-//   for ( var i = 0; i < cube_count; i ++ ) {
-//
-//     mesh4 = meshes[ i ];
-//
-//     mesh4.dx *= -1;
-//     mesh4.dy *= -1;
-//
-//   }
-//
-// }
-//
-// counter ++;
-// console.log(counter);
-///// end of distortion bits ... and here it gets weird
+
 
   renderer.render( scene, camera );
-
-/// ???? distortion
-//renderer.clear();
-//composer.render();
 }
--- move_files.py
+++ move_files.py
@@ -6,6 +6,12 @@ while True:
     if os.path.isfile("/Users/rolandarnoldt/Downloads/repl_left_nu.webm") is True:
         os.rename("/Users/rolandarnoldt/Downloads/repl_left_nu.webm", "/Users/rolandarnoldt/Documents/vidfun/repl_left_nu.webm")
         print("moved file left over")
+        # write to text file in vidfun folder video name
+        with open("/Users/rolandarnoldt/Documents/vidfun/move_vids.txt", 'w') as out:
+            out.write("file left")
     if os.path.isfile("/Users/rolandarnoldt/Downloads/repl_right_nu.webm") is True:
         os.rename("/Users/rolandarnoldt/Downloads/repl_right_nu.webm", "/Users/rolandarnoldt/Documents/vidfun/repl_right_nu.webm")
         print("moved file right over")
+        # write to text file in vidfun folder video name
+        with open("/Users/rolandarnoldt/Documents/vidfun/move_vids.txt", 'w') as out:
+            out.write("file right")
